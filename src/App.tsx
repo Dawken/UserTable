61235d3232c218e07275dc2ca23358f9b484d025
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-09-07 22:41:07 +0200

    refactor: delete unused react-query wrapper in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './app/routes/home/home'
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        },
-    },
-})
-
 const App = () => {
     return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
